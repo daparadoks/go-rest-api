e5ac8b1bf29b628a1544501d490d66c2a053a0a3
Yalçın Cengiz <yalcincengiz16@gmail.com>
2021-09-09 00:23:13 +0300

    jwt

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"strings"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 // Handler - stores pointer to our comments service
@@ -40,6 +42,62 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// BasicAuth - a handy middleware function that logs out incoming requests
+func BasicAuth(original func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if user == "admin" && pass == "password" && ok {
+			original(w, r)
+		} else {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			GetErrorResponseWithCode(w, "not authorized", 401)
+		}
+	}
+}
+
+// JWTAuth - a handy middleware function that will provide basic auth around specific endpoints
+func JWTAuth(original func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("jwt auth endpoint hit")
+		authHeader := r.Header["Authorization"]
+		if authHeader == nil {
+			SetHeaders(w)
+			GetErrorResponseWithCode(w, "not authorized", 401)
+		}
+
+		authHeaderParts := strings.Split(authHeader[0], " ")
+		if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
+			SetHeaders(w)
+			GetErrorResponseWithCode(w, "not authorized", 401)
+		}
+
+		if validateToken(authHeaderParts[1]) {
+			original(w, r)
+		} else {
+			SetHeaders(w)
+			GetErrorResponseWithCode(w, "not authorized", 401)
+		}
+	}
+}
+
+// validateToken - validates an incoming jwt token
+func validateToken(accessToken string) bool {
+	// replace this by loading in a private RSA cert for more security
+	var mySigningKey = []byte("missionimpossible")
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return mySigningKey, nil
+	})
+
+	if err != nil {
+		return false
+	}
+
+	return token.Valid
+}
+
 // SetupRoutes - sets up all the routes for our application
 func (h *Handler) SetupRoutes() {
 	fmt.Println("Setting up routes")
@@ -49,9 +107,9 @@ func (h *Handler) SetupRoutes() {
 	h.Router.HandleFunc("/api/comment", h.GetAllComments).Methods("GET")
 	h.Router.HandleFunc("/api/comment/{id}", h.GetComment).Methods("GET")
 
-	h.Router.HandleFunc("/api/comment", h.PostComment).Methods("POST")
-	h.Router.HandleFunc("/api/comment/{id}", h.UpdateComment).Methods("PUT")
-	h.Router.HandleFunc("/api/comment/{id}", h.DeleteComment).Methods("DELETE")
+	h.Router.HandleFunc("/api/comment", JWTAuth(h.PostComment)).Methods("POST")
+	h.Router.HandleFunc("/api/comment/{id}", BasicAuth(h.UpdateComment)).Methods("PUT")
+	h.Router.HandleFunc("/api/comment/{id}", BasicAuth(h.DeleteComment)).Methods("DELETE")
 
 	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -73,8 +131,12 @@ func SendOkResponse(w http.ResponseWriter, resp interface{}){
 	}
 }
 
-func GetErrorResponse(w http.ResponseWriter, message string) {
-	if err:= json.NewEncoder(w).Encode(Response{Success: false, Message: message, Code: http.StatusBadRequest});err!=nil {
+func GetErrorResponse(w http.ResponseWriter, message string){
+	GetErrorResponseWithCode(w, message, http.StatusBadRequest)
+}
+func GetErrorResponseWithCode(w http.ResponseWriter, message string, code int) {
+	w.WriteHeader(code)
+	if err:= json.NewEncoder(w).Encode(Response{Success: false, Message: message, Code: code});err!=nil {
 		panic(err)
 	}
 }
